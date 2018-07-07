508cd51dcd2a582b7d08ec355e03423def6dc42d
Kosukey <koshuke0217@icloud.com>
2018-07-07 14:03:11 +0900

    message form の作成done

--- pages/messages/new.js
+++ pages/messages/new.js
@@ -1,6 +1,26 @@
 import React, { Component }from 'react';
+import Layout from '../../components/Layout';
+import { Button, Form } from 'semantic-ui-react';
+import { Link } from '../../routes';
+import styled from 'styled-components';
 
-export default () => {
+class MessageForm extends Component {
 
-    return <h1>you can create new message at here</h1>;
+    render() {
+        return (
+            <Layout>
+                <div>
+                    <Form>
+                    <Form.Input fluid label='現在地' placeholder='東京都' />
+                    <Form.TextArea label='伝えたいこと' placeholder='Tell us more about you...' />
+                    <Form.Checkbox label='I agree to the Terms and Conditions' />
+                    <Form.Button>保存する</Form.Button>
+                </Form>
+                </div>
+            </Layout>
+
+        )
+    }
 }
+
+export default MessageForm;
