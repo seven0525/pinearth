e40f84e97bfdf018c7b67d0ce859e749957fcb62
Kosukey <koshuke0217@icloud.com>
2018-07-08 13:48:33 +0900

    get ido and keido detail values

--- pages/messages/new.js
+++ pages/messages/new.js
@@ -13,6 +13,80 @@ class MessageForm extends Component {
             alert( "あなたの端末では、現在位置を取得することができます。" ) ;
             console.log("あなたの端末では、現在位置を取得することができます");
 
+            // 現在地を取得
+            navigator.geolocation.getCurrentPosition(
+
+                // [第1引数] 取得に成功した場合の関数
+                function( position )
+                {
+                    // 取得したデータの整理
+                    var data = position.coords ;
+
+                    // データの整理
+                    var ido = data.latitude ;
+                    var keido = data.longitude ;
+                    var alt = data.altitude ;
+                    var accLatlng = data.accuracy ;
+                    var accAlt = data.altitudeAccuracy ;
+                    var heading = data.heading ;			//0=北,90=東,180=南,270=西
+                    var speed = data.speed ;
+
+                    // アラート表示
+			alert( "あなたの現在位置は、\n[" + ido + "," + keido + "]\nです。" ) ;
+
+                    // 位置情報
+                    // var latlng = new google.maps.LatLng( lat , lng ) ;
+
+                    // Google Mapsに書き出し
+                    // var map = new google.maps.Map( document.getElementById( 'map-canvas' ) , {
+                    //     zoom: 15 ,				// ズーム値
+                    //     center: latlng ,		// 中心座標 [latlng]
+                    // } ) ;
+
+                    // マーカーの新規出力
+                    // new google.maps.Marker( {
+                    //     map: map ,
+                    //     position: latlng ,
+                    // } ) ;
+                },
+
+                // [第2引数] 取得に失敗した場合の関数
+                function( error )
+                {
+                    // エラーコード(error.code)の番号
+                    // 0:UNKNOWN_ERROR				原因不明のエラー
+                    // 1:PERMISSION_DENIED			利用者が位置情報の取得を許可しなかった
+                    // 2:POSITION_UNAVAILABLE		電波状況などで位置情報が取得できなかった
+                    // 3:TIMEOUT					位置情報の取得に時間がかかり過ぎた…
+
+                    // エラー番号に対応したメッセージ
+                    var errorInfo = [
+                        "原因不明のエラーが発生しました…。" ,
+                        "位置情報の取得が許可されませんでした…。" ,
+                        "電波状況などで位置情報が取得できませんでした…。" ,
+                        "位置情報の取得に時間がかかり過ぎてタイムアウトしました…。"
+                    ] ;
+
+                    // エラー番号
+                    var errorNo = error.code ;
+
+                    // エラーメッセージ
+                    var errorMessage = "[エラー番号: " + errorNo + "]\n" + errorInfo[ errorNo ] ;
+
+                    // アラート表示
+                    alert( errorMessage ) ;
+
+                } ,
+
+                // [第3引数] オプション
+                {
+                    "enableHighAccuracy": false,
+                    "timeout": 8000,
+                    "maximumAge": 2000,
+                }
+
+            ) ;
+
         }
 
         else
