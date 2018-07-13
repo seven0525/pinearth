d733fa402df387ba507b4bbb5dfcc0726c46f2e6
Kosukey <koshuke0217@icloud.com>
2018-07-13 10:05:08 +0900

    自分のメッセージたちの緯度経度だけを配列で取得できるようにした

--- pages/users/mypage.js
+++ pages/users/mypage.js
@@ -22,6 +22,61 @@ if (!firebase.apps.length) {
 
 class MyPage extends Component {
 
+    state={
+        markerArray:''
+    }
+
+    componentWillMount() {
+
+
+
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+
+
+                const {currentUser} = firebase.auth();
+
+                var userId = currentUser.uid;
+
+
+                const firebaseMessagesRef = firebase.database().ref(`/messages`);
+
+                var mapPlaces = [];
+
+                var savedUserId = '';
+
+                firebaseMessagesRef
+                    .on("value", function (snapshot) {
+
+                        snapshot.forEach(function (childSnapshot) {
+
+                            const childData = childSnapshot.val();
+
+
+                            var savedIdo = childData.ido;
+
+                            var savedKeido = childData.keido;
+
+                            savedUserId = childData.postUserId;
+
+
+                            if (userId === savedUserId) {
+
+                                mapPlaces.push({ido: savedIdo, keido:savedKeido})
+                            }
+
+                        })
+
+
+                    })
+
+            }
+        })
+
+
+
+    }
+
 
     render() {
 
