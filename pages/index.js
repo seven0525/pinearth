b829341bdc3d097c6d04b64bac56b95a45f62536
Kosukey <koshuke0217@icloud.com>
2018-07-27 17:21:07 +0900

    sessionka

--- pages/index.js
+++ pages/index.js
@@ -172,6 +172,109 @@ class MessagesIndex extends Component {
 
     }
 
+    getMessagesArrayFromCache(place) {
+
+
+        var messages = [];
+
+        var message='';
+
+        var messagePlace = '';
+
+        var messageAuthor = '';
+
+        var messageAddress = '';
+
+        var messageId ='';
+
+        var messageEther = '';
+
+        var messageTransactionId = '';
+
+        var messageIpfsId ='';
+
+        var messagesPostDate = '';
+
+        var sortedArray = [];
+
+        var messagesEtherSentDate = '';
+
+        const here = place;
+
+
+        firebase.database().ref("/messages")
+
+            .on('value', snapshot => {
+                snapshot.forEach(function (childSnapshot) {
+
+                    const messagesData = childSnapshot.val();
+
+
+                    message = messagesData['message'];
+
+                    messagePlace = messagesData['place'];
+
+                    messageAuthor = messagesData['postUsername'];
+
+                    messageAddress = messagesData['postUserAddress'];
+
+                    messageId = messagesData['messageId'];
+
+                    messageEther = messagesData['amountEther'];
+
+                    messageTransactionId = messagesData['transactionId'];
+
+                    messageIpfsId = messagesData['ipfsId'];
+
+                    messagesPostDate =  messagesData['postDate'];
+
+                    messagesEtherSentDate =  messagesData['etherSentDate'];
+
+
+
+
+                    if (messageEther === undefined){
+
+                        messageEther = 0;
+
+                    }else{
+
+                        messageEther = messageEther*0.0001;
+
+                    }
+
+
+
+                    if( here === messagePlace) {
+
+                        messages.push({ message:message, place: messagePlace, author: messageAuthor, address:messageAddress,
+                            messageId: messageId, amountEther:messageEther, transactionId: messageTransactionId,
+                            ipfsId: messageIpfsId, postDate: messagesPostDate, etherSentDate: messagesEtherSentDate});
+
+                    }
+
+                })
+
+                // Ether量が多い記事が上に来るように降順にソート
+
+                sortedArray=
+                    messages.sort(function(a,b){
+
+
+                        return (a.etherSentDate>b.etherSentDate) ? -1 : 1;
+
+                    });
+
+                this.setState({messagesArray:sortedArray});
+
+            }).bind(this);
+
+
+    }
+
+
+
+
 
 
 
@@ -184,110 +287,130 @@ class MessagesIndex extends Component {
 
 
         var here = '';
-        if( navigator.geolocation )
-        {
-            // alert( "あなたの端末では、現在位置を取得することができます。" ) ;
-            // console.log("あなたの端末では、現在位置を取得することができます");
 
-            // 現在地を取得
-            navigator.geolocation.getCurrentPosition(
-                // [第1引数] 取得に成功した場合の関数
-                function (position) {
-                    // 取得したデータの整理
-                    var data = position.coords;
+        if( sessionStorage.getItem('place')==null) {
+            if (navigator.geolocation) {
+                // alert( "あなたの端末では、現在位置を取得することができます。" ) ;
+                // console.log("あなたの端末では、現在位置を取得することができます");
 
-                    // データの整理
-                    var ido = data.latitude;
-                    var keido = data.longitude;
-                    var alt = data.altitude;
-                    var accLatlng = data.accuracy;
-                    var accAlt = data.altitudeAccuracy;
-                    var heading = data.heading;			//0=北,90=東,180=南,270=西
-                    var speed = data.speed;
+                // 現在地を取得
+                navigator.geolocation.getCurrentPosition(
+                    // [第1引数] 取得に成功した場合の関数
+                    function (position) {
+                        // 取得したデータの整理
+                        var data = position.coords;
 
-                    hereThis.setState({ido: ido, keido:keido})
+                        // データの整理
+                        var ido = data.latitude;
+                        var keido = data.longitude;
+                        var alt = data.altitude;
+                        var accLatlng = data.accuracy;
+                        var accAlt = data.altitudeAccuracy;
+                        var heading = data.heading;			//0=北,90=東,180=南,270=西
+                        var speed = data.speed;
 
-                    // アラート表示
-                    // alert("あなたの現在位置は、\n[" + ido + "," + keido + "]\nです。");
+                        hereThis.setState({ido: ido, keido: keido})
 
-                    var apiKey = 'AIzaSyBjaU7Kz8PQ3gPIJmf70fm-Zvenjq9suT0';
+                        // アラート表示
+                        // alert("あなたの現在位置は、\n[" + ido + "," + keido + "]\nです。");
 
-                    var requestURL = 'https://maps.googleapis.com/maps/api/geocode/json?language=ja&sensor=false';
+                        var apiKey = 'AIzaSyBjaU7Kz8PQ3gPIJmf70fm-Zvenjq9suT0';
 
-                    requestURL += '&latlng=' + ido + ',' + keido;
-                    requestURL += '&key=' + apiKey;
+                        var requestURL = 'https://maps.googleapis.com/maps/api/geocode/json?language=ja&sensor=false';
+
+                        requestURL += '&latlng=' + ido + ',' + keido;
+                        requestURL += '&key=' + apiKey;
 
 
-                    fetch(requestURL)
-                        .then(response => response.json())
-                        .then(json => {
-                            here = json.results[0].formatted_address;
-                            const hereBigName = here.match("(.{2,3}[都道府県])");
-                            const hereSmallName = here.match("(.{1,3}[区市町])");
-                            const hereAllNames = here.match("(.{2,3}[都道府県].{1,3}[区市町])");
-                            var herePlaceNameWithSpace = ''
+                        fetch(requestURL)
+                            .then(response => response.json())
+                            .then(json => {
+                                here = json.results[0].formatted_address;
+                                const hereBigName = here.match("(.{2,3}[都道府県])");
+                                const hereSmallName = here.match("(.{1,3}[区市町])");
+                                const hereAllNames = here.match("(.{2,3}[都道府県].{1,3}[区市町])");
+                                var herePlaceNameWithSpace = ''
 
-                            //geolocation API は返して来る値が一定でないので全てに対応できるようにする
+                                //geolocation API は返して来る値が一定でないので全てに対応できるようにする
 
-                            if (hereAllNames === null || hereAllNames === '' ){
+                                if (hereAllNames === null || hereAllNames === '') {
 
-                                herePlaceNameWithSpace = hereBigName[0] + hereSmallName[0]
+                                    herePlaceNameWithSpace = hereBigName[0] + hereSmallName[0]
 
-                            }else{
+                                } else {
 
-                                herePlaceNameWithSpace = hereAllNames[0]
+                                    herePlaceNameWithSpace = hereAllNames[0]
 
-                            }
+                                }
 
-                            const herePlaceName = herePlaceNameWithSpace.replace(/\s+/g, '')
-                            hereThis.setState({place: herePlaceName});
-                            hereThis.setState({loading: false});
-                        }).then(()=>{
-                        hereThis.getMessagesArray();
-                    });
+                                const herePlaceName = herePlaceNameWithSpace.replace(/\s+/g, '')
+                                hereThis.setState({place: herePlaceName});
+                                hereThis.setState({loading: false});
+                                sessionStorage.setItem('place', herePlaceName);
+                                sessionStorage.setItem('ido', ido);
+                                sessionStorage.setItem('keido', keido);
 
-                },
 
-                // [第2引数] 取得に失敗した場合の関数
-                function (error) {
-                    // エラーコード(error.code)の番号
-                    // 0:UNKNOWN_ERROR				原因不明のエラー
-                    // 1:PERMISSION_DENIED			利用者が位置情報の取得を許可しなかった
-                    // 2:POSITION_UNAVAILABLE		電波状況などで位置情報が取得できなかった
-                    // 3:TIMEOUT					位置情報の取得に時間がかかり過ぎた…
+                            }).then(() => {
+                            hereThis.getMessagesArray();
+                        });
 
-                    // エラー番号に対応したメッセージ
-                    var errorInfo = [
-                        "原因不明のエラーが発生しました…。",
-                        "位置情報の取得が許可されませんでした…。",
-                        "電波状況などで位置情報が取得できませんでした…。",
-                        "位置情報の取得に時間がかかり過ぎてタイムアウトしました…。"
-                    ];
+                    },
 
-                    // エラー番号
-                    var errorNo = error.code;
+                    // [第2引数] 取得に失敗した場合の関数
+                    function (error) {
+                        // エラーコード(error.code)の番号
+                        // 0:UNKNOWN_ERROR				原因不明のエラー
+                        // 1:PERMISSION_DENIED			利用者が位置情報の取得を許可しなかった
+                        // 2:POSITION_UNAVAILABLE		電波状況などで位置情報が取得できなかった
+                        // 3:TIMEOUT					位置情報の取得に時間がかかり過ぎた…
 
-                    // エラーメッセージ
-                    var errorMessage = "[エラー番号: " + errorNo + "]\n" + errorInfo[errorNo];
+                        // エラー番号に対応したメッセージ
+                        var errorInfo = [
+                            "原因不明のエラーが発生しました…。",
+                            "位置情報の取得が許可されませんでした…。",
+                            "電波状況などで位置情報が取得できませんでした…。",
+                            "位置情報の取得に時間がかかり過ぎてタイムアウトしました…。"
+                        ];
 
-                    // アラート表示
-                    alert(errorMessage);
+                        // エラー番号
+                        var errorNo = error.code;
 
-                },
+                        // エラーメッセージ
+                        var errorMessage = "[エラー番号: " + errorNo + "]\n" + errorInfo[errorNo];
 
-                // [第3引数] オプション
-                {
-                    "enableHighAccuracy": false,
-                    "timeout": 8000,
-                    "maximumAge": 2000,
-                }
-            );
+                        // アラート表示
+                        alert(errorMessage);
 
-        }
+                    },
 
-        else {
-            alert("あなたの端末では、現在位置を取得できません。");
-            console.log("あなたの端末では、現在位置を取得できません");
+                    // [第3引数] オプション
+                    {
+                        "enableHighAccuracy": false,
+                        "timeout": 8000,
+                        "maximumAge": 2000,
+                    }
+                );
+
+            }
+
+            else {
+                alert("あなたの端末では、現在位置を取得できません。");
+                console.log("あなたの端末では、現在位置を取得できません");
+
+            }
+        }else{
+
+            var savedPlaceName =  sessionStorage.getItem('place');
+            var savedIdo = sessionStorage.getItem('ido');
+            var savedKeido = sessionStorage.getItem('keido');
+
+
+            console.log(savedPlaceName)
+
+            hereThis.setState({place: savedPlaceName, ido:savedIdo, keido:savedKeido});
+            hereThis.setState({loading: false});
+            hereThis.getMessagesArrayFromCache(savedPlaceName);
 
         }
     }
