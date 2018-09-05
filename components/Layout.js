834365363a9c8ccb0b89cfd07caf3a47e8eec4b4
Taichi Watanabe <seven0511pman@gmail.com>
2018-09-05 12:50:42 +0900

    Add files via upload

---
+++ components/Layout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from './Header';
+import { Container } from 'semantic-ui-react';
+import Head from 'next/head';
+
+export default props => {
+    return (
+        <Container>
+            <Head>
+                <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css"></link>
+                <meta name="viewport" content="width=device-width,initial-scale=1"></meta>
+            </Head>
+            <div style={containerStyle}>
+                <Header/>
+                {props.children}
+            </div>
+        </Container>
+    )
+}
+
+const containerStyle = {
+    position: "absolute",
+    left: "0px",
+    right: "0px"
+}
